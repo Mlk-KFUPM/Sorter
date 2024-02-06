6d5b3c7186fd13842dead6e7edb280c568cb3370
Mlk-KFUPM <112309991+Mlk-KFUPM@users.noreply.github.com>
2024-02-06 19:40:08 +0300

    Adding the selectionSort method.

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,6 +1,18 @@
 public class Sorter {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello2");
+    public static void selectionSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
     }
 }
